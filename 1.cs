4f61b1f82b0076d3e7758c3766076e1ec3c04de9
DeborahhRojass <61131474+DeborahhRojass@users.noreply.github.com>
2020-02-26 21:04:06 -0500

    CapturaRoja1

---
+++ 1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Déborah1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CaraDePlátano");
+        }
+    }
+}
